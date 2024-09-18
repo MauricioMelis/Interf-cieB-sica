1d2066dac3c4ea2ef1132cf884a457c9e64146b1
10047775 <mauriciosantiago.melis@uvic.cat>
2024-09-18 16:36:08 +0200

    Ejercicio 3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 
 namespace InterfícieBàsica
 {
@@ -12,11 +13,18 @@ namespace InterfícieBàsica
         private void Botó1_Click(object sender, RoutedEventArgs e)
         {
             Etiqueta.Content = "Text canviat!";
+            Botó2.Background = new SolidColorBrush(Colors.LightGreen);
+            Botó1.Background = new SolidColorBrush(Colors.Red);
+            Etiqueta.Foreground = new SolidColorBrush(Colors.Red);
+
         }
 
-        private void Botó2_Click(object sender, RoutedEventArgs e)
+        private void Botó2_Click_1(object sender, RoutedEventArgs e)
         {
             Etiqueta.Content = "Hola, món!";
+            Botó1.Background = new SolidColorBrush(Colors.LightGreen);
+            Botó2.Background = new SolidColorBrush(Colors.Red);
+            Etiqueta.Foreground = new SolidColorBrush(Colors.Green);
         }
     }
 }
