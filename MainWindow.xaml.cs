8a7b273c048e87550556b0d2940d754b9a553f11
10047775 <mauriciosantiago.melis@uvic.cat>
2024-09-18 15:32:53 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace InterfícieBàsica
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Botó1_Click(object sender, RoutedEventArgs e)
+        {
+            Etiqueta.Content = "Text canviat!";
+        }
+
+        private void Botó2_Click(object sender, RoutedEventArgs e)
+        {
+            Etiqueta.Content = "Hola, món!";
+        }
+    }
+}
